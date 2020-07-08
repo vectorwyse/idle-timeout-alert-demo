c2f790d261f00abaafd9470b32c88f13627de2dd
Jeffrey Li <jeffrey@vectorwyse.com>
2020-07-08 01:19:12 -0400

    require package assets

--- resources/js/app.js
+++ resources/js/app.js
@@ -20,6 +20,7 @@ window.Vue = require('vue');
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+require('./../assets/vendor/vectorwyse/idle-timeout-alert/js/app');
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
